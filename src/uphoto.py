fafd7237a9d64577a45e4e934f57914e5e47f62f
레오(오화종) <hwajong@coupang.com>
2014-07-28 13:37:34 +0900

    동작 방식 설명 추가

--- src/uphoto.py
+++ src/uphoto.py
@@ -38,8 +38,16 @@ FB_PASS = 'xxxxxxxxx'
 REDIRECT_URI = 'http://192.168.80.27:8000/'
 
 
+# 페이스북으로부터 ACCESS TOKEN 을 얻는다.
+#
+# 페이스북에 ACCESS TOKEN 을 요청하면 페이스북은 사용자 계정 인증후 요청시 전달한 REDIRECT URL로 ACCESS TOKEN 을 전달해 준다.
+# 본 프로그램의 경우 REDIRECT URL 에 해당하는 서버를 뛰어 놓지 않고 mechanize 브라우저의 내부동작을 트레이싱해
+# REDIRECT URL 로 전달되는 ACCESS TOKEN 을 켑처하는 방식으로 ACCESS TOKEN 을 얻는다.
 def get_access_token():
+
     # Set logger
+    # mechanize 브라우저의 내부동작을 트레이싱하기 위한 로그 설정
+    # mechanize 의 내부동작이 br.log 파일에 기록된다.
     logger = logging.getLogger("mechanize")
     logger.addHandler(logging.StreamHandler(open('br.log', 'wb')))
     logger.setLevel(logging.DEBUG)
@@ -47,15 +55,18 @@ def get_access_token():
     br = mechanize.Browser()
     br.set_handle_robots(False)
 
+    # 페이스북 인증요청 API url 설정
     fb_auth_url_prefix = 'https://www.facebook.com/dialog/oauth?scope=publish_actions&response_type=token'
     fb_auth_url = '%s&redirect_uri=%s&client_id=%s&client_secret=%s' % (fb_auth_url_prefix, REDIRECT_URI, APP_ID, APP_SECRET)
     print '>> fb_auth_url : ' + fb_auth_url
 
+    # mechanize 브라우저로 페이스북 인증요청
     br.open(fb_auth_url)
 
     # for f in br.forms():
     #      print f
 
+    # 페이스북 사용자 계정인증 (email/pass)
     br.form = list(br.forms())[0]
     control = br.form.find_control("email")
     control.value = FB_ID
@@ -66,14 +77,17 @@ def get_access_token():
         print '>> now submit !!!'
         br.submit()
     except URLError:
+        # mechanize 브라우저는 REDIRECT URL 로 접속 하려고 하지만 해당 서버는 없으므로 항상 URLError 가 발생한다 --> 무시.
         pass
 
+    # br.log 를 열어 기록된 ACCESS TOKEN 을 얻어온다.
     f = open('br.log', 'r')
     lines = f.readlines()
     f.close()
 
     token = None
 
+    # ACCESS TOKEN 파싱
     for line in lines:
         pos = line.find("access_token")
         if pos > 0:
@@ -96,6 +110,7 @@ if __name__ == '__main__':
         exit(-1)
 
     # upload photo
+    # GraphAPI 를 이용하여 페이스북으로 사진을 업로드한다.
     graph = facebook.GraphAPI(access_token)
     graph.put_photo(open('tomato.jpg', 'rb'), 'test', 'photos')
 
